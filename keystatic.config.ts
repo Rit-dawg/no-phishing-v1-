dde1ec0b4241bd4e03b5607198b8246e83c541f8
Rit-dawg <ritvickdogra112@gmail.com>
2026-01-12 22:22:43 +0530

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,7 +3,10 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+   repo: {
+     owner: Rit-dawg,
+     name: N-P-CMS
   },
   collections: {
     advisories: collection({
