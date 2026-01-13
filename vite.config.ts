d4a108e1c102d2ffadbd8631d174e84aea2299be
Rit-dawg <ritvickdogra112@gmail.com>
2026-01-13 21:36:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY || ""),
+      'process.env.ADMIN_HARDCODED': JSON.stringify(env.ADMIN_HARDCODED || ""),
     },
     build: {
       outDir: 'dist',
